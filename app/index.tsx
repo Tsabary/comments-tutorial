aafc403ce177f76d4331aa261ece30cb207830fb
Yanay Zabary <yanay.tsabary@gmail.com>
2025-01-28 13:22:02 +0200

    update home layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, FlatList, TouchableOpacity } from "react-native";
+import { posts } from "../constants/dummy-data";
 
 export default function HomeScreen() {
   return (
-    <View className="bg-red-500 flex-1">
-      <Text>Hello World</Text>
+    <View className="bg-gray-100 flex-1 p-4">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View className="bg-white p-4 mb-4 rounded-xl shadow-lg">
+            <Text className="text-lg font-bold text-gray-800">{item.id}</Text>
+            <Text className="text-gray-600 mt-2">{item.content}</Text>
+            <TouchableOpacity
+              className="bg-blue-500 mt-4 p-3 rounded-lg"
+              onPress={() => console.log(`Open comments for ${item.id}`)}
+            >
+              <Text className="text-white text-center font-medium">
+                Open Discussion
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
